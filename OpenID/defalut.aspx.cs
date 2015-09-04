a53a26e94246fd249b16fcd7292d93affa1b985b
unknown <0xfffffff7@gmail.com>
2015-09-04 17:56:14 +0900

    init

---
+++ OpenID/defalut.aspx.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.IO;
+using System.Configuration;
+
+namespace OpenID
+{
+    public partial class defalut : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            string openid = Request["openid"];
+            if (string.IsNullOrEmpty(openid) == false)
+            {
+
+                //-----------------------------------------------------------
+                // 直接要求でアソシエーションを確立する.
+                //-----------------------------------------------------------
+                string openidURL = ConfigurationManager.AppSettings["OPENID_ENDPOINT"];
+                string assoc_ns = "http://specs.openid.net/auth/2.0";
+                string assoc_mode = "associate";
+                string assoc_assoc_type = "HMAC-SHA256";
+                string assoc_session_type = "no-encryption";
+
+                openidURL += "?openid.ns=" + HttpUtility.UrlEncode(assoc_ns);
+                openidURL += "&openid.mode=" + assoc_mode;
+                openidURL += "&openid.assoc_type=" + assoc_assoc_type;
+                openidURL += "&openid.session_type=" + assoc_session_type;
+
+                System.Net.WebClient wc = new System.Net.WebClient();
+                Stream st = wc.OpenRead(openidURL);
+                wc.Dispose();
+
+                StreamReader sr = new StreamReader(st);
+                string resData = sr.ReadToEnd();
+                sr.Close();
+                st.Close();
+
+
+                //-----------------------------------------------------------
+                // アソシエーションレスポンスのサンプル.
+                //-----------------------------------------------------------
+                // ns:http://specs.openid.net/auth/2.0
+                // assoc_handle:....
+                // session_type:no-encryption
+                // assoc_type:HMAC-SHA256
+                // expires_in:14400
+                // mac_key:....
+
+
+                //-----------------------------------------------------------
+                // アソシエーションハンドルと共有鍵をセッションに保存.
+                //-----------------------------------------------------------
+                string[] stArrayData = resData.Split('\n');
+                foreach (string stData in stArrayData){
+                    string[] keyValueData = stData.Split(':');
+                    Session[keyValueData[0]] = keyValueData[1];
+                }
+
+
+                //-----------------------------------------------------------
+                // 間接要求で認証リダイレクトを行う.
+                //-----------------------------------------------------------
+                openidURL = ConfigurationManager.AppSettings["OPENID_ENDPOINT"];
+                string claimed_id = "http://specs.openid.net/auth/2.0/identifier_select";
+                string identity = "http://specs.openid.net/auth/2.0/identifier_select";
+                string mode = "checkid_setup";
+                string ns = "http://specs.openid.net/auth/2.0";
+                string realm = ConfigurationManager.AppSettings["SELF_DOMAIN"];
+                string return_to = ConfigurationManager.AppSettings["CALLBACK_URL"];
+                string assoc_handle = Session["assoc_handle"].ToString();
+
+                // URL作成.
+                openidURL += "?openid.claimed_id=" + claimed_id;
+                openidURL += "&openid.identity=" + HttpUtility.UrlEncode(identity);
+                openidURL += "&openid.mode=" + mode;
+                openidURL += "&openid.ns=" + HttpUtility.UrlEncode(ns);
+                openidURL += "&openid.realm=" + HttpUtility.UrlEncode(realm);
+                openidURL += "&openid.return_to=" + HttpUtility.UrlEncode(return_to);
+                openidURL += "&openid.assoc_handle=" + assoc_handle;
+                
+
+                Response.Redirect(openidURL);
+
+            }
+        }
+    }
+}
---
+++ OpenID/openid.aspx.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Security.Cryptography;
+using System.Configuration;
+
+namespace OpenID
+{
+    public partial class openid : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            // レスポンスのサンプル
+            // openid.ns=http://specs.openid.net/auth/2.0
+            // openid.mode=id_res
+            // openid.return_to=https://要求時に指定した自身のコールバックURL
+            // openid.claimed_id=https://me.yahoo.com/a/..........
+            // openid.identity=https://me.yahoo.com/a/.......
+            // openid.assoc_handle=.........
+            // openid.realm=https://自身のドメイン
+            // openid.response_nonce=.........
+            // openid.signed=assoc_handle,claimed_id,identity,mode,ns,op_endpoint,response_nonce,return_to,signed,pape.auth_level.nist
+            // openid.op_endpoint=https://open.login.yahooapis.com/openid/op/auth
+            // openid.pape.auth_level.nist=0
+            // openid.sig=.........
+
+
+            // レスポンス表示
+            foreach (string key in Request.QueryString.Keys)
+            {
+                LABEL1.Text += string.Format("{0}={1}<br><br>", key, Request.QueryString[key]);
+            }
+
+
+            // アサーションの検証.
+            //
+            // 1. openid.return_toが現在の要求の URL と一致する
+            // 2. ディスカバリによって得られた情報がアサーションの情報と一致する
+            //  　主張識別子 openid.claimed_id 
+            //　  OPローカル識別子 openid.identity 
+            //　  OPエンドポイントURL openid.op_endpoint 
+            //　  プロトコルバージョン openid.ns 
+            // 3. openid.response_nonce について、当該 OP から、これまでに同じ値のアサーションを受け入れたことがない
+            // 4. アサーションの署名が有効で、署名が必要な全てのフィールドに署名がされている
+            
+            // ここでは1と4だけ検証する.
+
+            // コールバックURL検証
+            if (Request.QueryString["openid.return_to"].ToString() != ConfigurationManager.AppSettings["CALLBACK_URL"]){
+                LABEL1.Text += "<br/><br/>Verify Failed!!<br/><br/><br/><br/><br/><br/>";
+                return;
+            }
+
+            // シグネチャ検証検証
+            // openid.signedに書かれている順番でキーバリュー形式に変換する。
+            string keyValue = string.Empty;
+            string signed = Request.QueryString["openid.signed"];
+
+            string[] stArrayData = signed.Split(',');
+            foreach (string stData in stArrayData)
+            {
+                keyValue += stData + ":" + Request.QueryString["openid." + stData] + "\n";
+            }
+
+            LABEL1.Text += "<hr>";
+            LABEL1.Text += "<br/><br/>Verify signature parameter=" + keyValue;
+
+            LABEL1.Text += "<hr>";
+
+            // 共有鍵を使用してHMACを計算する。
+            byte[] bytes = System.Text.Encoding.UTF8.GetBytes(keyValue);
+            string macKey = Session["mac_key"].ToString();
+            HMACSHA256 signer = new HMACSHA256(Convert.FromBase64String(macKey));
+            byte[] result = signer.ComputeHash(bytes);
+
+            LABEL1.Text += "<br/><br/>openid.sig=" + Request.QueryString["openid.sig"].ToString();
+            LABEL1.Text += "<br/>Compute signature=";
+            string computeSignature = System.Convert.ToBase64String(result);
+            LABEL1.Text += computeSignature;
+
+            if (Request.QueryString["openid.sig"].ToString() == computeSignature)
+            {
+                LABEL1.Text += "<br/><br/>Verify Success!!<br/><br/><br/><br/><br/><br/>";
+            }
+            else
+            {
+                LABEL1.Text += "<br/><br/>Verify Failed!!<br/><br/><br/><br/><br/><br/>";
+            }
+            
+
+        }
+
+    }
+}
